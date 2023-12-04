823b3a941c9ef39c607a5ba16900ce8acbc8d559
Nguyen Khanh <nguyenkhanh@Nguyens-Mac-mini.local>
2023-12-04 22:09:53 +0700

    feat: sort by name & any number column

--- src/Consumers/consumerColumnsConfig.ts
+++ src/Consumers/consumerColumnsConfig.ts
@@ -2,8 +2,15 @@
 
 export type ConsumerDto = {
     id: number
+    fullname: string
     amount_spent: number
     amount_incompleted: number
+}
+
+const makeNumberSorter = (field: string) => {
+    return (a: any, b: any) => {
+        return a[field] - b[field]
+    }
 }
 
 export const columnsConfig = [
@@ -15,9 +22,11 @@ export const columnsConfig = [
     {
         title: 'Tên',
         key: 'name',
-        // filterMode: 'menu',
-        // filterSearch: true,
-        // onFilter: (value, record: any) => record.name.startsWith(value),
+        sorter: (a: ConsumerDto, b: ConsumerDto) =>{
+            const nameA = a.fullname.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.fullname.toUpperCase(); // ignore upper and lowercase
+            return nameA < nameB ? -1 : (nameA > nameB ? 1 : 0)
+        },
     },
     {
         title: 'Điện thoại',
@@ -28,22 +37,24 @@ export const columnsConfig = [
         key: 'amount_spent',
         responsive: ['md'],
         defaultSortOrder: 'descend',
-        sorter: (a: ConsumerDto, b: ConsumerDto) => a.amount_spent - b.amount_spent,
+        sorter: makeNumberSorter('amount_spent'),
         sortDirections: ['descend'],
     },
     {
         title: 'Đơn hoàn thành',
         key: 'count_completed_orders',
+        sorter: makeNumberSorter('count_completed_orders'),
     },
     {
         title: 'Cọc',
         key: 'amount_incompleted',
         responsive: ['md'],
-        sorter: (a: ConsumerDto, b: ConsumerDto) => a.amount_incompleted - b.amount_incompleted,
+        sorter: makeNumberSorter('amount_incompleted'),
     },
     {
         title: 'Đơn chưa',
         key: 'count_incompleted_orders',
+        sorter: makeNumberSorter('count_incompleted_orders'),
     },
     {
         title: 'Địa chỉ',
